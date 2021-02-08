a6796a6cffd6e33e01c7e42bc65bef0db59fe098
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2021-02-08 19:13:28 +0100

    App design finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {SharedModule} from './shared/shared.module';
+import {GifsModule} from './gifs/gifs.module';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+ ],
   imports: [
-    BrowserModule
+    BrowserModule, SharedModule, GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
