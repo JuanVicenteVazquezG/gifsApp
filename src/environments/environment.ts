c65ac4a03c4ca70e67666c02a4ef894901accb16
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2021-02-08 21:42:16 +0100

    test giphy api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_key: '1Y3JsY2SNhktcdeU4a8oJQsG9wOV11Ac'
 };
 
 /*
