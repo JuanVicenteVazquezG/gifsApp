3ce4da2394e6a12af9c430345e9b64073f0a596f
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2021-02-09 09:01:11 +0100

    http module, api proofs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
+
 import {SharedModule} from './shared/shared.module';
 import {GifsModule} from './gifs/gifs.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
  ],
   imports: [
-    BrowserModule, SharedModule, GifsModule
+    BrowserModule, SharedModule, GifsModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,27 +10,32 @@ export class GifsService {
 
   // tslint:disable-next-line:variable-name
   private api_key = environment.api_key;
+  // tslint:disable-next-line:variable-name
   private _record: string[] = [];
+  public results: any[] = [];
 
   get record(): string[] {
     return [...this._record];
   }
 
-  async searchGifs(query: string): void {
-    if (query.trim().length > 0) {
-      if (this._record.indexOf(query) === -1) {
-        this._record.unshift(query);
-        if (this._record.length > 10) {
-          this._record.splice(-1, 1);
-        }
+  constructor(private http: HttpClient) {
+  }
+
+  searchGifs(query: string): void {
+    query = query.trim().toLocaleLowerCase();
+    if (this._record.indexOf(query) === -1) {
+      this._record.unshift(query);
+      if (this._record.length > 10) {
+        this._record.splice(-1, 1);
       }
-      const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${this.api_key}&q=${query}&limit=10`);
-      const data = await res.json();
-      console.log(data);
-      //   .then(res => res.json().then(data => console.log(data)))
-      //   .catch(error => {
-      //     console.log(error);
-      //   });
+      this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.api_key}&q=${query}&limit=10`)
+        .pipe(
+          map((resp: any) => {
+            const {data} = resp;
+            return data;
+          }),
+        )
+        .subscribe((data: any) => this.results = data);
     }
   }
 
